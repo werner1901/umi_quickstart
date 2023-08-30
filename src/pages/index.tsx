0c5219a2a1d14a179966e7b20c9c16216cb2a8b9
田悦廷 <werner1901@163.com>
2023-08-30 10:51:59 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  UserListPage from "./Users/index"
+import './index.less'
+export default () => {
+  return (
+    <div className='test'>
+      <UserListPage></UserListPage>
+    </div>
+  );
+}
